fbb2c4b42c31c09f29f8ac6e00829476ab5f20ec
NataGildry <natalia.podpryatova@gmail.com>
2020-07-01 16:55:29 +0300

    Initial commit

---
+++ src/components/Main/MainContent.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const MainContent =() => {
+    return (
+        <div className="main">
+            <div>
+                <img src="https://media-cdn.tripadvisor.com/media/photo-s/16/ff/b2/f3/ic-5-24.jpg" alt=""/>
+            </div>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTHZ6GOe3OJT-itGwpgTisGjN1f-nHPmBO2yg&usqp=CAU" alt=""/>
+            </div>
+            <div>
+                My posts
+                <div>
+                    New posts
+                </div>
+                <div>post 1</div>
+                <div>post2</div>
+            </div>
+        </div>
+    );
+};
+
+export default MainContent;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar =() => {
+    return (
+        <div className="nav">
+        <div>
+            Profile
+        </div>
+        <div>
+        Messages
+        </div>
+    <div>
+            News
+        </div>
+            <div>
+                Music
+            </div>
+            <div>
+                Settings
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
